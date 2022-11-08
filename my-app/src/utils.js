196ff44a1b21c1448c1bf38f86eba6de3ea31889
smackmhard <104869058+smackmhard@users.noreply.github.com>
2022-11-07 19:06:25 -0500

    copy class chall, updated npm

---
+++ my-app/src/utils.js
@@ -0,0 +1,23 @@
+// hello = () => 'hello world'
+
+// document.getElementById("demo").innerHTML = hello();
+const apiKey = "";
+const button = document.getElementById("button");
+const input = document.getElementById("input");
+
+button.addEventListener("click", function() {
+
+const getMoviesByName = async (title) => {
+    const result = await fetch(`http://www.omdbapi.com/?t=${title}&apikey=${apiKey}&`);
+    const movie = await result.json();
+    console.log (movie);
+};
+demo.innerHTML = getMoviesByName('');
+});
+
+const getMoviesById = async (ID) => {
+    const result = await fetch(`http://www.omdbapi.com/?i=${ID}&apikey=${apiKey}&`);
+    const movie = await result.json();
+    console.log (movie);
+};
+getMoviesById('tt0094963');

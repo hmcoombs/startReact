879e20a954465bd7b41c74667ed8f5a0451134c3
smackmhard <104869058+smackmhard@users.noreply.github.com>
2022-11-13 17:06:36 -0500

    add more components

--- my-app/src/components/movieCard.jsx
+++ my-app/src/components/movieCard.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import './movieCard.css';
+
 
 export default function MovieCard({ title, type, posterUrl }) {
     return (
       <div className="movie-card">
-        <img src="#" alt="Batman" />
+        <img src={posterUrl} alt="Batman" />
         <div className="card-body">
           <h3>Batman v Something</h3>
         </div>
         <button>Movie</button>
       </div>
     );
+    
   }
 
   MovieCard.defaultProps = {
     title: '',
   };
-  
+  console.log(MovieCard);
+
+
